e391b0332f8fe8ebcc60eba72c79e15900284dbc
Konrad Basta <86340967+Mafia193@users.noreply.github.com>
2022-10-27 13:32:17 +0200

    Added time request

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 import http.server
 import socketserver
-import os
 import time
 
-
 class web_server(http.server.SimpleHTTPRequestHandler):
-    
-    def do_GET(self):
 
-        print(self.path)
-        
-        if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-        else:
-            super().do_GET()
-    
+	def do_header(self):
+		self.protocol_version = 'HTTP/1.1'
+		self.send_response(200)
+		self.send_header("Content-type", "text/html; charset=UTF-8")
+		self.end_headers()
+
+	def do_GET(self):
+		print(self.path)
+
+		if self.path == '/':
+			self.do_header()
+			self.wfile.write(b"Hello World!\n")
+		elif self.path.startswith('/cmd=time'):
+			self.do_header()
+			now = time.localtime()
+			current_time = time.strftime("%H:%M:%S", now)
+			self.wfile.write(current_time.encode("utf-8"))
+		else:
+			super().do_GET()
+
 # --- main ---
 
 PORT = 4080
 
 print(f'Starting: http://localhost:{PORT}')
 
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server = socketserver.TCPServer(("", PORT), web_server)
 tcp_server.serve_forever()
