3efd8443447479d364881370cfb00a5254276dfd
Konrad Basta <86340967+Mafia193@users.noreply.github.com>
2022-11-10 12:44:18 +0100

    Implemented signs counter

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -13,7 +13,21 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 
 	def do_GET(self):
 		print(self.path)
-		super().do_GET()
+
+		if self.path[0] == '/':
+			self.do_header()
+			str = self.path.split('&str=')
+			text = str[len(str) - 1]
+			if len(text) <= 1:
+				return 
+			lower = sum(1 for c in text if c.islower())
+			upper = sum(1 for c in text if c.isupper())
+			digit = sum(1 for c in text if c.isdigit())
+			space = sum(1 for c in text if c.isspace())
+			other = len(text) - upper - lower - digit - space
+			self.wfile.write(bytes("{{ \"lowercase\" : {}, \"uppercase\" : {}, \"digits\" : {}, \"special\" : {}}}".format(lower, upper, digit, other), self.encoding))
+		else:
+			super().do_GET()
 
 # --- main ---
 
